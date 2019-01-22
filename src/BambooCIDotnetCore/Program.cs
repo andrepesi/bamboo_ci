bdbeeb7fc732487757d543cb9aa08d51320b1b74
Andre Silva <andre.silva@nuance.com>
2019-01-22 13:33:03 -0200

    -- teste

--- src/BambooCIDotnetCore/Program.cs
+++ src/BambooCIDotnetCore/Program.cs
@@ -7,6 +7,7 @@ namespace BambooCIDotnetCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! 2");
         }
     }
 }
